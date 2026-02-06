4ac712fdd831310e216d9d67ac49ef607c5e8ffe
hitzby <45208586@qq.com>
2026-02-06 14:33:35 +0800

    second commit: add sorting algorithms implementations

---
+++ sort/insert.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#define MAX 100
+void insert_sort(int arr[],int n){
+    int i,j;
+    for(i=1;i<n;i++){
+        int key=arr[i];
+        for(j=i;j>0&&arr[j-1]>key;j--){
+            arr[j]=arr[j-1];
+        }
+        arr[j]=key;
+    }
+}
+int main(void){
+    int arr[MAX]={0};
+    int n;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    insert_sort(arr,n);
+    for(int i=0;i<n;i++){
+        printf(" %d ",arr[i]);
+    }
+    return 0;
+}
